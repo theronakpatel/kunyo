e412d8ba13c64fec3ddc884ac4dcb6641003b14a
Ronak Patel <eronax59@gmail.com>
2024-02-22 04:34:02 -0800

    Update order.php

--- class/order.php
+++ class/order.php
@@ -69,17 +69,22 @@ class order
 	 * @param  float $currency_input
 	 * @return float
 	 */
-	function convertCurrency(string $currency_from, string $currency_to, float $currency_input)
+	function convertCurrency(string $currency_from, string $currency_to, float $currency_input): float
 	{
 		$yql_base_url = "http://query.yahooapis.com/v1/public/yql";
-		$yql_query = 'select * from yahoo.finance.xchange where pair in ("' . $currency_from . $currency_to . '")';
-		$yql_query_url = $yql_base_url . "?q=" . urlencode($yql_query);
-		$yql_query_url .= "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-		$yql_session = curl_init($yql_query_url);
-		curl_setopt($yql_session, CURLOPT_RETURNTRANSFER, true);
-		$yqlexec = curl_exec($yql_session);
-		$yql_json =  json_decode($yqlexec, true);
-		return (float) $currency_input * $yql_json['query']['results']['rate']['Rate'];
+		$pair = $currency_from . $currency_to;
+		$query = 'select * from yahoo.finance.xchange where pair in ("' . $pair . '")';
+		$query_url = $yql_base_url . "?q=" . urlencode($query) . "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+		
+		$curl = curl_init($query_url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($curl);
+		curl_close($curl);
+		
+		$json_data = json_decode($response, true);
+		$rate = $json_data['query']['results']['rate']['Rate'];
+		
+		return (float) $currency_input * $rate;
 	}
 
 	/**
