0989bc3ee6342f479effde72e90db308a65f9dd9
Ronak Patel <eronax59@gmail.com>
2024-02-22 09:54:17 -0800

    Update order.php

--- class/order.php
+++ class/order.php
@@ -20,7 +20,7 @@ class order
 
 		if (mysqli_connect_errno()) {
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-			exit();
+			exit(); 
 		}
 	}
 	function addReward(object $order)
